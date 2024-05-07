5c4f91fb2687365d957762574fc51496589aa640
Anh La <anh.la@bellevuecollege.edu>
2024-05-07 00:11:21 -0700

    First upload to Github

---
+++ leetcode.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class leetcode {
+    public static void main(String[] args) {
+        System.out.println("Hello Anh");
+    }
+
+
+    // public static String encrypt (String plainText, int shift) {
+    //     // char c;
+    //     int a = (char) 'c';
+    //     for (int i = 0; i < plainText.length(); i++) {
+    //         char current = plainText.charAt(i);
+    //     }
+    //     return "";
+    // }
+}
