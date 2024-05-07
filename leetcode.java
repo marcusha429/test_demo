3a0e4b1381cb6f6c9744a6af8a0f2f6b45e2a421
Nguyen Ha <nguyeh3@uci.edu>
2024-05-07 00:28:46 -0700

    fibonacci

--- leetcode.java
+++ leetcode.java
@@ -3,6 +3,17 @@ import java.util.Arrays;
 class leetcode {
     public static void main(String[] args) {
         System.out.println("Hello Anh");
+        System.out.println("Fibonancci code");
+        int n = 10;
+        System.out.println("Fibonacci number at position " + n + " is: " + fibonacci(n));
+    }
+    
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
     }
 
 
